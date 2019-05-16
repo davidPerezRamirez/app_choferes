a43bbaeb888c4b03798e2833143eb169404fe0c7
Unknown <pucarauntref@gmail.com>
2019-05-15 22:07:53 -0300

    agrega firma de metodo GET para obtener las lista de gastos actuales para el usuario logueado

--- app/src/main/java/com/example/app_choferes/constants/ConstantRestApi.java
+++ app/src/main/java/com/example/app_choferes/constants/ConstantRestApi.java
@@ -5,4 +5,6 @@ public class ConstantRestApi {
     public static final String ROOT_URL_API_REST = "http://app-sindicato.herokuapp.com/";
 
     public static final String GET_USERS = "getUsers/";
+
+    public static final String LIST_EXPENSES_CURRENT_USER = "listExpensesFromCurrentUser/";
 }
--- app/src/main/java/com/example/app_choferes/service/QueriesRestAPIService.java
+++ app/src/main/java/com/example/app_choferes/service/QueriesRestAPIService.java
@@ -1,16 +1,23 @@
 package com.example.app_choferes.service;
 
 import com.example.app_choferes.constants.ConstantRestApi;
+import com.example.app_choferes.models.Expense;
 import com.example.app_choferes.models.User;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 
 public interface QueriesRestAPIService {
 
     @GET(ConstantRestApi.GET_USERS)
     Call<List<User>> userList();
+
+    @FormUrlEncoded
+    @GET(ConstantRestApi.LIST_EXPENSES_CURRENT_USER)
+    Call<List<Expense>> listExpensesFromCurrentUser(@Field("idUser")int idUser);
 
 }
