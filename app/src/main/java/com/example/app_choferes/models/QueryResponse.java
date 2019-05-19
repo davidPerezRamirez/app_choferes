94e481dca4a342a24164db9302957189bc60a2c0
Unknown <pucarauntref@gmail.com>
2019-05-19 18:44:21 -0300

    agrega clase para interpretar respuesta desde backend

---
+++ app/src/main/java/com/example/app_choferes/models/QueryResponse.java
@@ -0,0 +1,16 @@
+package com.example.app_choferes.models;
+
+import java.util.Map;
+
+public class QueryResponse {
+
+    private Map<String, String> result;
+
+    public QueryResponse(Map<String, String> respone) {
+        result = respone;
+    }
+
+    public boolean isSuccess() {
+        return Integer.parseInt(this.result.get("result")) == 1;
+    }
+}
